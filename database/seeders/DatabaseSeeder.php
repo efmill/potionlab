dda5dd81484860642e453c89722792c2778c57cd
fatsushiroll <emilyfairmiller@gmail.com>
2025-09-19 11:39:32 +0200

    Creating new models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+    
+        \App\Models\User::factory(10)->create();
+        \App\Models\Potion::factory(10)->create();
+        \App\Models\Ingredient::factory(10)->create();
     }
 }
